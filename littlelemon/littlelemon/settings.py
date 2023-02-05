a80a89a4fed77038d03ad03aa2d26a6c1456bf2c
DJay <diegovt.arbt@gmail.com>
2023-02-05 13:12:03 -0600

    Create: Booking and Reservation models

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
         'PORT': '3306',
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
